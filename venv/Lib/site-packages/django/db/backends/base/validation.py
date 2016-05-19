cec90b8a40ac75f2e9c546bd50c1a9b0ee9fba7a
upczww@gmail.com <upczww@gmail.com>
2016-05-19 13:47:04 +0800

    delete venv

--- venv/Lib/site-packages/django/db/backends/base/validation.py
+++
@@ -1,9 +0,0 @@
-class BaseDatabaseValidation(object):
-    """
-    This class encapsulates all backend-specific model validation.
-    """
-    def __init__(self, connection):
-        self.connection = connection
-
-    def check_field(self, field, **kwargs):
-        return []
