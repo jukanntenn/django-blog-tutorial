255bf256f88c7bfcdd149fda360af1a78dd2e54c
yangxg <zmrenwu@163.com>
2016-05-23 21:32:21 +0800

    Revert "Feature"

--- usera/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UseraConfig(AppConfig):
-    name = 'usera'
--- usera/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from usera import views
-
-urlpatterns = [
-    url(r'^signin/$', views.SignInView.as_view(), name='signin'),
-    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
-]
