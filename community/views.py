1ff86a2839b75547f5660166b432b33c84ad86e6
yangxueguang <415468651@qq.com>
2016-05-30 22:24:55 +0800

    NEW 添加发帖和回帖视图函数

--- community/views.py
+++ community/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from community.forms import PostForm, ReplyForm
+
 
 # Create your views here.
+class PostCreateView(FormView):
+    template_name = ''
+    form_class = PostForm
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        kwargs = super(PostCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(PostCreateView, self).form_valid(form)
+
+
+class ReplyCreateView(FormView):
+    template_name = ''
+    form_class = ReplyForm
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        kwargs = super(ReplyCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(ReplyCreateView, self).form_valid(form)
