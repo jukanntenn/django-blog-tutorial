2deedc1366801792bbb76c6ddc9f5715f46b77d7
Baldwin <dnnsun@gmail.com>
2016-05-31 14:04:50 +0800

    NEW - 基本简单的归档功能实现 :) ENH - 添加.gitignore文件以便提交时过滤一些不必要的本地资源。 ENH - 基本实现了标签字段的开发，标签云需要独立页面呈现并预计下次提交。 FIX - 修改readme关于部署的一些遗漏的节 :)

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,8 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Article(models.Model):
     STATUS_CHOICES = (
-        ('d', 'Draft'),
-        ('p', 'Published'),
+        ('d', "草稿"),
+        ('p', "已发布"),
     )
 
     title = models.CharField('标题', max_length=70)
@@ -22,7 +22,8 @@ class Article(models.Model):
     likes = models.PositiveIntegerField('点赞数', default=0)
     topped = models.BooleanField('置顶', default=False)
 
-    category = models.ForeignKey('Category', verbose_name='分类', null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', verbose_name='所属分类', null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField('Tag', verbose_name='标签集合', null=True, blank=True, help_text='标签')
 
     def __str__(self):
         return self.title
@@ -38,8 +39,16 @@ class Article(models.Model):
         ordering = ['-last_modified_time']
 
 
+class Tag(models.Model):
+    name = models.CharField('名称', max_length=20)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_modified_time = models.DateTimeField('修改时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
-    name = models.CharField('类名', max_length=20)
+    name = models.CharField('名称', max_length=20)
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
     last_modified_time = models.DateTimeField('修改时间', auto_now=True)
 
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
-    url(r'^category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^blog/$', views.IndexView.as_view(), name='index'),
+    url(r'^blog/article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^blog/category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<tag_name>[\w,-]+)$', views.TagView.as_view(), name='tag'), 
+    url(r'^archives', views.ArchiveView.as_view(), name='archives'),   
 ]
