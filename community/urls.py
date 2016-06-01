6010fc39c832b911dd4bd2b638c6d60d31fefae7
yangxueguang <415468651@qq.com>
2016-06-01 18:34:47 +0800

    Refactor community 各模块

--- community/urls.py
+++ community/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import url
 from community import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^post/create/$', views.PostCreateView.as_view(), name='post_create'),
+    url(r'^post-create/$', views.PostCreateView.as_view(), name='post_create'),
     url(r'^community/$', views.IndexView.as_view(), name='index'),
     url(r'^post/(?P<post_id>\d+)$', views.PostDetailView.as_view(), name='detail'),
-    # url(r'^category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
 ]
