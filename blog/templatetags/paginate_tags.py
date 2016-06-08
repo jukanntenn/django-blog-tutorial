4b9b5e01a8d4d01a96361b2cdd39b27b1966baca
JFluo <luojianfeng2011@163.com>
2016-06-08 14:48:42 +0800

    对paginate_tags.py代码进行简化

--- blog/templatetags/paginate_tags.py
+++ blog/templatetags/paginate_tags.py
@@ -14,45 +14,31 @@ def paginate(context, object_list, page_count):
 
     try:
         object_list = paginator.page(page)
-        context['current_page'] = int(page)
-        pages = get_left(context['current_page'], left, paginator.num_pages) + get_right(context['current_page'], right,
-                                                                                         paginator.num_pages)
     except PageNotAnInteger:
-        object_list = paginator.page(1)
-        context['current_page'] = 1
-        pages = get_right(context['current_page'], right, paginator.num_pages)
+        page = 1
     except EmptyPage:
-        object_list = paginator.page(paginator.num_pages)
-        context['current_page'] = paginator.num_pages
-        pages = get_left(context['current_page'], left, paginator.num_pages)
+        page = paginator.num_pages
+    finally:
+        if isinstance(page, int):
+            object_list = paginator.page(page)
+        context['current_page'] = int(page)
+        pages = get_pages(context['current_page'], left, right, paginator.num_pages)
 
     context['article_list'] = object_list
     context['pages'] = pages
     context['last_page'] = paginator.num_pages
     context['first_page'] = 1
-    try:
-        context['pages_first'] = pages[0]
-        context['pages_last'] = pages[-1] + 1
-    except IndexError:
-        context['pages_first'] = 1
-        context['pages_last'] = 2
+    context['pages_first'] = pages[0] if pages else 1
+    context['pages_last'] = pages[-1] + 1 if pages else 2
 
     return ''  # 必须加这个，否则首页会显示个None
 
 
-def get_left(current_page, left, num_pages):
-    if current_page == 1:
-        return []
-    elif current_page == num_pages:
-        l = [i - 1 for i in range(current_page, current_page - left, -1) if i - 1 > 1]
-        l.sort()
-        return l
-    l = [i for i in range(current_page, current_page - left, -1) if i > 1]
-    l.sort()
-    return l
+def get_pages(current_page, left, right, num_pages):
+    page = current_page - 1 if current_page == num_pages else current_page
 
-
-def get_right(current_page, right, num_pages):
-    if current_page == num_pages:
-        return []
-    return [i + 1 for i in range(current_page, current_page + right - 1) if i < num_pages - 1]
+    pages = (
+        [i for i in range(page - left + 1, page + 1) if i > 1] +
+        [i + 1 for i in range(current_page, current_page + right - 1) if i < num_pages - 1]
+    )
+    return pages
