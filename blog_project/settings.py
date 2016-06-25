a9dcdcd1cb32161e1e5450ab8cefe73d53b493ac
yangxueguang <415468651@qq.com>
2016-06-25 21:16:23 +0800

    成功合并，添加 gitignore 文件

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- manage.py
+++ manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog_project.settings")
-=======
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_project.settings")
->>>>>>> origin/blog-tutorial
 
     from django.core.management import execute_from_command_line
 
