cb58c98b47f82510deaa1569547d651b52c67059
Lucky <175917645@qq.com>
2016-06-28 22:19:52 +0800

    修改follow model，定义到一张表中，去掉follow_nums和is_follow

--- apps/follow/models.py
+++ apps/follow/models.py
@@ -13,23 +13,10 @@ class Follow(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     follow_num = models.IntegerField(default = 0)
-
+    user = models.ForeignKey(settinggs.AUTH_USER_MODEL)
+    follow_time = models.DateTimeField(auto_now = True)
     def __str__(self):
         return '%s: %s(%s)' % (self.content_type, self.object_id, self.follow_num)
 
-'''
-follow详情
-@param {Boolean} is_follow　是否关注
-@param {Date} follow_time 关注的时间
-@param {Object}　外键关联Follow类
-@param {Object} 外键关联，登陆用户
-'''
-class FollowDetail(models.Model):
-    is_follow = models.BooleanField(default = False)
-    follow_time = models.DateTimeField(auto_now = True)
-    follows = models.ForeignKey(Follow)
-    user = models.ForeignKey(settinggs.AUTH_USER_MODEL)
-
     class Meta:
         ordering = ['-follow_time']
-
