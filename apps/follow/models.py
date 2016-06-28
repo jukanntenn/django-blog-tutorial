f693ad393fab8384b81fb28b82794a79eae0e04b
Lucky <175917645@qq.com>
2016-06-29 00:34:47 +0800

    add view's follow method

--- apps/follow/models.py
+++ apps/follow/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from config import settinggs
+from config import settings
 
 # Create your models here.
 # 关注帖子的功能,同时也可以扩展为其他关注的功能
@@ -13,7 +13,7 @@ class Follow(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     follow_num = models.IntegerField(default = 0)
-    user = models.ForeignKey(settinggs.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     follow_time = models.DateTimeField(auto_now = True)
     def __str__(self):
         return '%s: %s(%s)' % (self.content_type, self.object_id, self.follow_num)
