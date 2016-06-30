cc43300ca27ff2448f51fc16e0d21758cd0b40dc
Lucky <175917645@qq.com>
2016-07-01 00:32:52 +0800

    继续修改view

--- apps/follow/views.py
+++ apps/follow/views.py
@@ -4,13 +4,11 @@ from django.http import HttpResponse
 # 关注某个帖子
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.contenttypes.models import ContentType
-from django.template import RequestContext
-from django.views.generic.edit import CreateView
 
 from apps.notifications.signals import notify
 from .models import Follow
 
-def follow_change(self, request):
+def follow_change(request, pk):
 
     '''
     handle follow state
@@ -18,16 +16,25 @@ def follow_change(self, request):
     @param {object_pk} Object pk
     @param {state} follow or unfollow
     '''
+    '''
+    
     ctype_pk = self.request.GET.get('content_type_id')
     object_pk = self.request.GET.get('object_pk')
     state = True if request.GET.get('state') == True else False
-
     content_type = get_object_or_404(ContentType, pk = int(ctype_pk))
     self.content_object = content_type.get_object_for_this_type(pk = int(object_pk))
-        
+    '''
+
+    target = get_object_or_404(ContentType, pk = pk)
+    content_type = ContentType.get_object_for_this_type(target)
+
+
     # get followers
+    '''
     try:
-        followers = Follow.objects.get(content_type = content_type, object_pk = object_pk)
+        followers = Follow.objects.get(content_type = content_type, object_id = pk)
     except Exception:
         # no followers, then create one
-        followers = Follow(content_type = content_type, object_pk = object_pk)
+        followers = Follow(content_type = content_type, object_id = pk)
+    '''
+    # followed = Follow.objects.get_or_create(content_type = content_type, object_id = pk)
