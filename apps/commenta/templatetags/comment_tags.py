f385cf6bf2d59fc175d2953adb260398ca66bcec
yangxueguang <415468651@qq.com>
2016-07-01 19:26:37 +0800

    首页和详情页正确显示相关信息

--- apps/commenta/templatetags/comment_tags.py
+++ apps/commenta/templatetags/comment_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.contrib.contenttypes.models import ContentType
 from ..forms import CommentForm
 from ..models import Comment
+from django.utils.timesince import timesince
 
 register = template.Library()
 
@@ -28,3 +29,20 @@ def comment_count(obj):
             object_id=obj.pk,
             content_type=ContentType.objects.get_for_model(obj)
     ).count()
+
+
+@register.filter
+def last_comment_by(obj):
+    return Comment.objects.filter(
+            object_id=obj.pk,
+            content_type=ContentType.objects.get_for_model(obj)
+    ).order_by('-modified_time').first().author.username
+
+
+@register.filter
+def last_comment_timesince(obj):
+    last_comment = Comment.objects.filter(
+            object_id=obj.pk,
+            content_type=ContentType.objects.get_for_model(obj)
+    ).order_by('-modified_time').first()
+    return timesince(last_comment.modified_time)
