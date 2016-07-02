ae8fcdc82e5484323b496490bc00f2c2bfa297d9
yangxueguang <415468651@qq.com>
2016-07-02 09:49:03 +0800

    添加 follow model

--- apps/follow/models.py
+++ apps/follow/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from config import settings
 
 
 # Create your models here.
 # 关注帖子的功能,同时也可以扩展为其他关注的功能
+class Follow(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='followed_objects')
+    follow_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s followed %s' % (self.user, self.content_object)
+
+    class Meta:
+        ordering = ['-follow_time']
--- apps/follow/views.py
+++ apps/follow/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 # 关注某个帖子
+def follow(request, content_type_id, object_id):
+    # A view for follow any object.
+    # if already followed , cancel follow it
+    pass
