d906427eccdea358d01251001f5cda727afdd472
zmrenwu <zmrenwu@163.com>
2016-08-20 11:23:07 +0800

    Modify fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -47,9 +47,9 @@ def _update_static_files(source_folder):
 
 
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 manage.py makemigrations' %
+    run('cd %s && ../env/bin/python3 manage.py makemigrations' %
         (source_folder,))
-    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' %
+    run('cd %s && ../env/bin/python3 manage.py migrate --noinput' %
         (source_folder,))
 
 
