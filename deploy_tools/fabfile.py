eccf9cc89c6b904a48ce898a948a30bf696c9158
zmrenwu <zmrenwu@163.com>
2016-08-20 11:12:13 +0800

    Remove static media migrations from vcs

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,20 +5,32 @@ import random
 REPO_URL = 'https://github.com/djangoStudyTeam/DjangoBlog.git'
 
 
+def deploy():
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+
 def _create_directory_structure_if_necessary(site_folder):
     run('mkdir -p %s/%s' % (site_folder, 'env'))
     run('mkdir -p %s/%s' % (site_folder, 'source'))
-    for sub_folder in ('source/weblog/database', 'source/weblog/static'):
-        run('mkdir -p %s/%s' % (site_folder, sub_folder))
 
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
         run('cd %s && git fetch' % (source_folder,))
     else:
-        run('git clone %s %s' % (REPO_URL, source_folder))
+        run('git clone %s %s && git checkout blog-tutorial' % (REPO_URL, source_folder))
     current_commit = local('git log -n 1 --format=%H', capture=True)
-    run('cd %s && git reset --hard %s && git checkout blog-tutorial' % (source_folder, current_commit))
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+    for sub_folder in (
+            'weblog/database', 'weblog/static', 'weblog/media'):
+        run('mkdir -p %s/%s' % (source_folder, sub_folder))
 
 
 def _update_virtualenv(source_folder):
