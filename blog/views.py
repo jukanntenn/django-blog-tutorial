75cc34043a8bfed9448d2fa127d917b5c9ff177d
yangxg <zmrenwu@163.com>
2017-03-18 19:28:53 +0800

    Step5: blog index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+"""
+使用下方的模板引擎方式。
+def index(request):
+    return HttpResponse("欢迎访问我的博客首页！")
+"""
+
+
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'blogproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
