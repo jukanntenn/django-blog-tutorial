144494e391e11225d02e6b51b3f8f34d93b5e406
yangxg <zmrenwu@163.com>
2017-03-20 22:28:12 +0800

    Step6: real blog index view

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 
+from .models import Post
+
 """
 使用下方的模板引擎方式。
 def index(request):
     return HttpResponse("欢迎访问我的博客首页！")
 """
 
-
+"""
+使用下方真正的首页视图函数
 def index(request):
     return render(request, 'blog/index.html', context={
         'title': '我的博客首页',
         'welcome': '欢迎访问我的博客首页'
     })
+"""
+
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={'post_list': post_list})
