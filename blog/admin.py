5a576fce2d4bfb1a1504b165937d14de6777c7d7
yangxg <zmrenwu@163.com>
2017-03-22 20:54:04 +0800

    Step7: publish posts using django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
