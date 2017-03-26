4d98a0710205f8a513157fd96df976128088ed32
yangxg <zmrenwu@163.com>
2017-03-26 11:37:35 +0800

    Step11: category and archive

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ import markdown
 
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Category
 
 """
 使用下方的模板引擎方式。
@@ -34,3 +34,14 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
