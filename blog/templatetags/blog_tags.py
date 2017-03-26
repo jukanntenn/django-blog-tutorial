e9b078da695d1b6e7f948e6046ac516f3f007255
yangxg <zmrenwu@163.com>
2017-03-26 11:08:04 +0800

    Step10: side bar

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,20 @@
+from django import template
+
+from ..models import Post, Category
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all()[:num]
+
+
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
