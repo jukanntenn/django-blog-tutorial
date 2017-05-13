c69372951ed8342ed40b607d21abed019cba2035
yangxg <zmrenwu@163.com>
2017-05-13 16:37:40 +0800

    完善注释，新的 detail、base 模板，新的样式文件。

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
 
 """
-使用下方的模板引擎方式。
+请使用下方的模板引擎方式。
 def index(request):
     return HttpResponse("欢迎访问我的博客首页！")
 """
 
 """
-使用下方真正的首页视图函数
+请使用下方真正的首页视图函数
 def index(request):
     return render(request, 'blog/index.html', context={
         'title': '我的博客首页',
@@ -19,7 +18,7 @@ def index(request):
 
 
 def index(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
