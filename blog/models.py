8e7439557b0b8e69f0e4237890c7e0fd395ffde7
yangxg <zmrenwu@163.com>
2017-05-13 15:25:10 +0800

    完善注释，新的 index.html 模板，新的样式文件。

--- blog/models.py
+++ blog/models.py
@@ -3,41 +3,33 @@ from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 
 
+# python_2_unicode_compatible 装饰器用于兼容 Python2
 @python_2_unicode_compatible
 class Category(models.Model):
     """
-    django 要求我们必须继承 models.Model 类，
+    Django 要求模型必须继承 models.Model 类。
     Category 只需要一个简单的分类名 name 就可以了。
-
-    CharField 指定了 name 的数据类型，
-    CharField 是字符型，
-    max_length 指定其最大长度，
-    超过这个长度的分类名就不能被存入数据库。
-
-    当然 django 还为我们提供了各种各样的类型，
-    如日期时间类型 DateTimeField、
-    整数类型 IntegerField 等等。
-    django 内置的类型全部类型可查看文档：
+    CharField 指定了分类名 name 的数据类型，CharField 是字符型，
+    CharField 的 max_length 参数指定其最大长度，超过这个长度的分类名就不能被存入数据库。
+    当然 Django 还为我们提供了多种其它的数据类型，如日期时间类型 DateTimeField、整数类型 IntegerField 等等。
+    Django 内置的全部类型可查看文档：
     https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
     """
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        # python_2_unicode_compatible 装饰器用于兼容 Python2
         return self.name
 
 
 @python_2_unicode_compatible
 class Tag(models.Model):
     """
-    标签 Tag 也比较简单，
-    和 Category 一样。
+    标签 Tag 也比较简单，和 Category 一样。
     再次强调一定要继承 models.Model 类！
     """
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        # python_2_unicode_compatible 装饰器用于兼容 Python2
         return self.name
 
 
@@ -51,56 +43,32 @@ class Post(models.Model):
     title = models.CharField(max_length=70)
 
     # 文章正文，我们使用了 TextField。
-    # 比较短的字符串存储可以使用 CharField，
-    # 但对于文章的正文来说可能会是一大段文本，
-    # 因此使用 TextField 来存储大段文本。
+    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
     body = models.TextField()
 
-    # 这两个列分表表示了文章的创建时间和最后一次修改时间，
-    # 存储时间的列用 DateTimeField。
+    # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
 
-    # 文章摘要，可以没有文章摘要，
-    # 但默认情况下 CharField 要求我们必须存入数据，
-    # 否则就会报错。
-    # 指定 blank=True 后就可以允许空值了。
+    # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
+    # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
     excerpt = models.CharField(max_length=200, blank=True)
 
-    # 这是分类与标签，
-    # 分类与标签的模型我们已经定义在上面。
-    # 我们在这里把文章对应的数据库表和分类与标签对应的表关联起来，
-    # 但是关联形式稍微有点不同。
-    # 我们规定一篇文章只能对应一个分类，
-    # 但是一个分类下可以有很多篇文章，
-    # 所以我们使用的是 ForeignKey，
-    # 即一对多的关系。
-    # 而对于标签来说，
-    # 一篇文章可以有多个标签，
-    # 同一个标签下也可能有多篇文章，
-    # 所以我们使用 ManyToManyField，
-    # 表明这是多对多的关系。
-    # 同时我们规定文章可以没有标签，
-    # 因此为标签 tags 指定了 blank=True。
-    # 如果你对 ForeignKey、ManyToManyField 不了解，
-    # 请看教程中的解释，
-    # 亦可参考官方文档：
+    # 这是分类与标签，分类与标签的模型我们已经定义在上面。
+    # 我们在这里把文章对应的数据库表和分类、标签对应的数据库表关联了起来，但是关联形式稍微有点不同。
+    # 我们规定一篇文章只能对应一个分类，但是一个分类下可以有多篇文章，所以我们使用的是 ForeignKey，即一对多的关联关系。
+    # 而对于标签来说，一篇文章可以有多个标签，同一个标签下也可能有多篇文章，所以我们使用 ManyToManyField，表明这是多对多的关联关系。
+    # 同时我们规定文章可以没有标签，因此为标签 tags 指定了 blank=True。
+    # 如果你对 ForeignKey、ManyToManyField 不了解，请看教程中的解释，亦可参考官方文档：
     # https://docs.djangoproject.com/en/1.10/topics/db/models/#relationships
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
 
-    # 文章作者
-    # 这里 User 是从 django.contrib.auth.models 导入的。
-    # django.contrib.auth 是 django 内置的应用，
-    # 专门用于处理网站用户的注册、登录等流程，
-    # User 是 django 为我们已经写好的用户模型，
-    # 这里我们通过 ForeignKey 把文章和 User 关联起来，
-    # 因为我们规定一篇文章只能有一个作者，
-    # 而一个作者可能会写多篇文章，
-    # 因此这是一对多的关系，
-    # 和 Category 类似。
+    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
+    # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
 
     def __str__(self):
-        # python_2_unicode_compatible 装饰器用于兼容 Python2
         return self.title
