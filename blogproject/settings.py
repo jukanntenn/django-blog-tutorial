6df893eae57e79dac31fcbe8374b06c8b2009991
yangxg <zmrenwu@163.com>
2017-05-17 18:55:06 +0800

    使用 CDN 加速静态资源的加载。

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'i3k%m-808v3_)^h7975iw4v&fl5chq41^19j@u+b*vx7dvw*q$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.zmrenwu.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
 
 # Application definition
 
