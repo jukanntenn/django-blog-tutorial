bbf0dbbd3bf5d9b23779f42dee29b455b2c82846
yangxg <zmrenwu@163.com>
2017-05-17 19:34:32 +0800

    Step16: record post views

--- blog/views.py
+++ blog/views.py
@@ -41,6 +41,10 @@ def detail(request, pk):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    # 阅读量 +1
+    post.increase_views()
+
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
