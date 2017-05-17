4c45c582849c85ba560fc9f5e499a07e526fe837
yangxg <zmrenwu@163.com>
2017-05-17 19:15:47 +0800

    新增 fabfile 部署脚本

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import env, run
+from fabric.operations import sudo
+
+GIT_REPO = "you git repository"
+
+env.user = 'you host username'
+env.password = 'you host password'
+
+# 填写你自己的主机对应的域名
+env.hosts = ['demo.zmrenwu.com']
+
+# 一般情况下为 22 端口，如果非 22 端口请查看你的主机服务提供商提供的信息
+env.port = '22'
+
+
+def deploy():
+    source_folder = '/home/yangxg/sites/zmrenwu.com/django-blog-tutorial'
+
+    run('cd %s && git pull' % source_folder)
+    run("""
+        cd {} &&
+        ../env/bin/pip install -r requirements.txt &&
+        ../env/bin/python3 manage.py collectstatic --noinput &&
+        ../env/bin/python3 manage.py migrate
+        """.format(source_folder))
+    sudo('restart gunicorn-demo.zmrenwu.com')
+    sudo('service nginx reload')
