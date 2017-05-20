5b66f3bfe835035d550df28c67786d9dfad27373
yangxg <zmrenwu@163.com>
2017-05-20 16:14:56 +0800

    添加 robots 规则

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
+    url(r'^robots\.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
 ]
